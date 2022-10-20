24c8ef8536ad87e99130deeb8ba9b95a7a5c9eb8
root-dm <proangel99@gmail.com>
2022-10-20 20:57:04 +0300

    deleted unnecessary code

--- udp/client.py
+++ udp/client.py
@@ -12,11 +12,8 @@ def listener():
             data=""
 
 serverAddressPort   = ("127.0.0.1", 5050)
-
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# threading.Thread(target=listener(UDPClientSocket)).start()
 threading.Thread(target=listener).start()
-
 
 message = input("\x1b[6;30;42mEpiloges:\x1b[0m \n Signin <username>\n-> ")
 while not message.startswith('Signin'):
--- udp/server.py
+++ udp/server.py
@@ -1,21 +1,3 @@
-# import socketserver
-# import threading
-
-# ServerAddress = ("127.0.0.1", 5050)
-
-# class handler(socketserver.DatagramRequestHandler):
-#     def handle(self):
-
-#         print("Recieved one request from {}".format(self.client_address[0]))
-#         datagram = self.rfile.readline().strip()
-#         print("Datagram Recieved from client is:".format(datagram))
-#         print(datagram)  
-#         self.wfile.write("Message from Server! Hello Client".encode())
-
-# UDPServerObject = socketserver.ThreadingUDPServer(ServerAddress, handler)
-# UDPServerObject.serve_forever()
-from email import message
-import random
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -25,7 +7,6 @@ registered=[]
 
 def new_client(message, caddr):
     print("Connection from: " + str(caddr[0]+":"+str(caddr[1])))
-    # while True:
     data = message
     if str(data).startswith('Signin'):
         name = str(data).replace('Signin ', '')
@@ -45,17 +26,13 @@ def new_client(message, caddr):
                 registered.remove(user)
                 data="\n\033[92m(!)\033[0m Signed out, goodbye \033[92m"+user["name"]+"\033[0m"
                 server_socket.sendto(data.encode(), user["ip"])
-                # csocket.send("Event: signed out".encode())
-                # csocket.close()
 
                 callback_message="\n\033[92m(!)\033[0m \033[92m"+user["name"]+"\033[0m has signed out"
                 for user in registered:
                     if user["ip"] != caddr:
                         server_socket.sendto(callback_message.encode(), user["ip"])
     elif str(data).startswith('Message'):
-    # else:
         message = str(data).replace('Message ', '')
-        # message = str(data)
         callback_message=""
         cnt_received=0
         delivered=False
