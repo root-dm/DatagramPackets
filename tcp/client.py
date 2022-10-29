f2e649ef70e8fd320b13b892ae49baaf13146004
root-dm <proangel99@gmail.com>
2022-10-29 17:54:28 +0300

    file search

--- tcp/client.py
+++ tcp/client.py
@@ -1,7 +1,15 @@
 from asyncio import sleep
+import os
 import socket
 import threading
 import time
+
+def filelist(path):
+    filelist = ""
+    files = os.listdir(path)
+    for file in files:
+        filelist += file + ","
+    return filelist.rstrip(filelist[-1])
 
 def listener():
     while True:
@@ -20,12 +28,14 @@ client_socket.connect((host, port))
 
 threading.Thread(target=listener).start()
 
-message = input("\x1b[6;30;42mEpiloges:\x1b[0m \n Signin <username>\n-> ")
+message = input("\x1b[6;30;42mEpiloges:\x1b[0m \n Signin\n-> ")
 while not message.startswith('Signin'):
-    message = input("\x1b[6;30;42mEpiloges:\x1b[0m \n Signin <username>\n-> ")
+    message = input("\x1b[6;30;42mEpiloges:\x1b[0m \n Signin\n-> ")
 
+list = filelist('D:/Documents/GitHub/python-socket/tcp')
+message=message+","+list
 client_socket.send(message.encode())
-print ("\x1b[6;30;42mEpiloges:\x1b[0m \n Signout\n Message <message>\n")
+print ("\x1b[6;30;42mEpiloges:\x1b[0m \n Signout\n Search <keywords>\n")
 while True:
     message = input("")
     client_socket.send(message.encode())
--- tcp/server.py
+++ tcp/server.py
@@ -11,56 +11,27 @@ def new_client(csocket, caddr):
         if not data:
             break
         if str(data).startswith('Signin'):
-            name = str(data).replace('Signin ', '')
+            filelist = str(data).replace('Signin,', '')
+            filelist = filelist.split(',')
             ip = str(str(caddr[0])+":"+str(caddr[1]))
-            user={"name": name, "ip": ip, "socket": csocket}
+            user={"ip": ip, "files": filelist, "socket": csocket}
             registered.append(user)
-            data="\n\033[92m(!)\033[0m Registered, welcome \033[92m"+name+"\033[0m"
-            print (ip+" / " + name +" - registered!")
-            csocket.send(data.encode())
-
-            callback_message="\n\033[92m(!)\033[0m \033[92m"+name+"\033[0m has signed in"
-            for user in registered:
-                if user["ip"] != str(str(caddr[0])+":"+str(caddr[1])):
-                    user["socket"].send(callback_message.encode())
         elif str(data).startswith('Signout'):
             for user in registered:
                 if user["ip"]==str(str(caddr[0])+":"+str(caddr[1])):
                     registered.remove(user)
-                    data="\n\033[92m(!)\033[0m Signed out, goodbye \033[92m"+user["name"]+"\033[0m"
-                    csocket.send(data.encode())
-                    csocket.send("Event: signed out".encode())
-                    csocket.close()
-
-                    callback_message="\n\033[92m(!)\033[0m \033[92m"+user["name"]+"\033[0m has signed out"
-                    for user in registered:
-                        if user["ip"] != str(str(caddr[0])+":"+str(caddr[1])):
-                            user["socket"].send(callback_message.encode())
-        #elif str(data).startswith('Message'):
-        else:
-            # print ("Message handler\n----") #DEBUG
-            # message = str(data).replace('Message ', '')
-            message = str(data)
-            callback_message=NULL
-            cnt_received=0
-            delivered=False
+        elif str(data).startswith('Search'):
+            message = str(data).replace('Search ', '')
+            keywords = message.split()
+            callback_message="Results\n"
             for user in registered:
-                if user["ip"] == str(str(caddr[0])+":"+str(caddr[1])):
-                    callback_message="\033[1m"+user["name"]+":\033[0m "+message
+                print (user["files"])
+                for file in user["files"]:
+                    for keyword in keywords:
+                        if keyword in file:
+                            callback_message+=file+":"+user['ip']+"\n"
 
-            for user in registered:
-                if user["ip"] != str(str(caddr[0])+":"+str(caddr[1])):
-                    user["socket"].send(callback_message.encode())
-                    cnt_received+=1
-                    delivered=True
-            if cnt_received==0:
-                msg="\n\033[91m(!)\033[0m No users online\n"
-                csocket.send(msg.encode())
-            if delivered==False:
-                msg="\n\033[91m(!)\033[0m Message not delivered\n"
-                csocket.send(msg.encode())
-        # else:
-        #     csocket.send("Invalid command".encode())
+            csocket.send(callback_message.encode())
 
 
 server_socket = socket.socket()
